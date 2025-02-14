c165db9587b12d8c1785c1deb39d4644bd9896c0
Daniel García <workspacedaniel2121@gmail.com>
2025-02-14 22:15:41 +0100

    Initial State

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
-
-import AuthorTitle from './components/AuthorTitle'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import VideoGallery from './components/VideoGallery'
-import About from './components/About'
-import Contact from './components/Contact'
-
 const App = () => {
-  return (
-    <>
-      <header className='p-6 flex justify-between items-center'>
-        <AuthorTitle />
-        <Navbar />
-      </header>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/portfolio' element={<VideoGallery />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
-    </>
-  )
+  return <div className='text-3xl underline'>App</div>
 }
 
 export default App
