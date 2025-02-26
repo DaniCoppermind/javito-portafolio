7104e6a0d5c284c796a218edb94fb37a4bd881df
Daniel García <workspacedaniel2121@gmail.com>
2025-02-26 12:30:41 +0100

    Actualizar configuraciones para desarrollo local y ajustar rutas de API

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from './axios';
 
-const API = 'https://javito-portafolio-backend.onrender.com/api';
+const API = 'http://localhost:4000/api';
 
 export const loginRequest = async user => {
   return await axios.post(`${API}/login`, user, { withCredentials: true });
