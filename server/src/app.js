bd3d9e5971c8ee73e505bd88fc9bf0a4d8152a65
Daniel García <workspacedaniel2121@gmail.com>
2025-03-01 19:01:45 +0100

    añade compresión Gzip en el servidor.

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,16 @@ import express from 'express'
 import morgan from 'morgan'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import compression from 'compression'
 
 import dashboardRoutes from './routes/dashboard.routes.js'
 import authRoutes from './routes/auth.routes.js'
 import videosRoutes from './routes/videos.routes.js'
 
 const app = express()
+
+//  Gzip Compresión
+app.use(compression())
 
 app.use(
   cors({
@@ -18,6 +22,8 @@ app.use(
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
+
+app.use(express.static('public'))
 
 app.use('/api', authRoutes)
 app.use('/api', dashboardRoutes)
