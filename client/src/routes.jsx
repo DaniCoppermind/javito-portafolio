47e22a5f14b164648cff4c5ce82de74367f9e622
Daniel García <workspacedaniel2121@gmail.com>
2025-03-01 19:00:49 +0100

    Refactor rutas protegidas: elimina la verificación de carga y utiliza una variable de entorno para el token de autenticación.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -3,16 +3,15 @@ import { useAuth } from './context/AuthContext';
 import Cookies from 'js-cookie';
 
 export function ProtectedRoute() {
-  const { isAuthenticated, loading } = useAuth();
+  const { isAuthenticated } = useAuth();
 
-  if (loading) return <h1>Loading...</h1>;
-  if (!isAuthenticated && !loading) return <Navigate to="/" replace />;
+  if (!isAuthenticated) return <Navigate to="/" replace />;
 
   return <Outlet />;
 }
 
 export function ProtectedLoginRoute() {
-  const token = Cookies.get('loginSecure');
+  const token = Cookies.get(import.meta.env.VITE_TOKEN_NAME);
   const secretValue = import.meta.env.VITE_SECRET_KEY;
   return token === secretValue ? <Outlet /> : <Navigate to="/" />;
 }
