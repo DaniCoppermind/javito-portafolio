115a1cf4d2018dabf6af6a4a049a5682aee9e95a
Daniel <workspacedaniel2121@gmail.com>
2025-03-04 20:43:05 +0100

    Navbar fix

--- server/src/app.js
+++ server/src/app.js
@@ -1,32 +1,32 @@
-import express from 'express'
-import morgan from 'morgan'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import compression from 'compression'
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import compression from 'compression';
 
-import dashboardRoutes from './routes/dashboard.routes.js'
-import authRoutes from './routes/auth.routes.js'
-import videosRoutes from './routes/videos.routes.js'
+import dashboardRoutes from './routes/dashboard.routes.js';
+import authRoutes from './routes/auth.routes.js';
+import videosRoutes from './routes/videos.routes.js';
 
-const app = express()
+const app = express();
 
 //  Gzip Compresión
-app.use(compression())
+app.use(compression());
 
 app.use(
   cors({
-    origin: 'https://javito-portafolio-frontend.onrender.com',
+    origin: 'http://localhost:5173',
     credentials: true,
   })
-)
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(cookieParser())
+);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.use('/api', authRoutes)
-app.use('/api', dashboardRoutes)
-app.use('/api', videosRoutes)
+app.use('/api', authRoutes);
+app.use('/api', dashboardRoutes);
+app.use('/api', videosRoutes);
 
-export default app
+export default app;
