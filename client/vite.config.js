2938c44170c143e9633c87480d75d30e55ce3df0
Daniel <workspacedaniel2121@gmail.com>
2025-03-11 13:21:40 +0100

    render origin

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://javito-portafolio-backend.onrender.com',
         changeOrigin: true,
       },
     },
